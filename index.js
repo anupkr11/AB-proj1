8bf3ac1393bb82b08cfc70f8ccf00682761b17ed
anupkr11 <anup94622@gmail.com>
2025-11-02 12:26:00 +0530

    firstCommit

---
+++ index.js
@@ -0,0 +1,9 @@
+import { header } from "./components/header.js";
+// import { navbar } from "./components/navbar.js";
+// import { main } from "./components/main.js";
+// import { footer } from "./components/footer.js";
+
+document.getElementById("root").innerHTML = `
+  ${header}
+
+`;
