14a8b2a714a0a95dc7f4b8a9c409c240e71d6cb6
anupkr11 <anup94622@gmail.com>
2025-11-03 12:36:30 +0530

    added footer

---
+++ components/footer.js
@@ -0,0 +1,28 @@
+export const footer = `
+<footer class="bg-black text-white py-10">
+  <div class="flex flex-col items-center space-y-4">
+
+    <img src="assets/footer-logo.png" alt="Logo" class="w-32">
+
+    <div class="flex space-x-6 mt-4">
+      <a href="#" class="text-white hover:text-green-500 transition">
+        <i class="fab fa-facebook-f text-2xl"></i>
+      </a>
+      <a href="#" class="text-white hover:text-green-500 transition">
+        <i class="fab fa-twitter text-2xl"></i>
+      </a>
+      <a href="#" class="text-white hover:text-green-500 transition">
+        <i class="fab fa-pinterest text-2xl"></i>
+      </a>
+      <a href="#" class="text-white hover:text-green-500 transition">
+        <i class="fab fa-instagram text-2xl"></i>
+      </a>
+    </div>
+
+    <p class="text-sm text-gray-400 mt-4">
+      Energico © 2022 · Privacy Policy</a>
+    </p>
+  </div>
+</footer>
+
+`
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { header } from "./components/header.js";
 import { navbar } from "./components/navbar.js";
 import { home } from "./components/home.js";
-// import { footer } from "./components/footer.js";
+import { footer } from "./components/footer.js";
 
 document.getElementById("root").innerHTML = `
   ${header}
   ${navbar}
   ${home}
+  ${footer}
 
 `;
